2c5ecaddab2c7ec275b57a5c406fd1f43b891433
Alexander Wallau <alexander.wallau@gmx.net>
2022-11-07 14:40:29 +0100

    Add Sound from mayNiklas/nixos

---
+++ modules/sound/default.nix
@@ -0,0 +1,23 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.sound;
+in
+{
+
+  options.mayniklas.sound = { enable = mkEnableOption "activate sound"; };
+
+  config = mkIf cfg.enable {
+
+    # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio = {
+      enable = true;
+      package = pkgs.pulseaudioFull;
+    };
+
+    environment.systemPackages = with pkgs; [ pavucontrol ];
+
+    users.extraUsers.${config.awallau.var.mainUser}.extraGroups = [ "audio" ];
+
+  };
+}
