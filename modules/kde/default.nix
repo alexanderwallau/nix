b79cbd674d95a9bafc6b22f2b25bed2bf7e8ca94
alexanderwallau <alexander.wallau@gmx.net>
2023-06-11 16:03:52 +0200

    Renamed more options for new version

--- modules/kde/default.nix
+++ modules/kde/default.nix
@@ -17,19 +17,18 @@ in
       displayManager.sddm.enable = true;
       desktopManager.plasma5 = {
         enable = true;
-        # exclude some packages from plasma5
-        excludePackages = with pkgs.libsForQt5; [
-          elisa
-          gwenview
+       
+      };
+    };
+     # exclude some packages from plasma5
+    environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+      gwenview
           oxygen
           khelpcenter
           # konsole # we should not remove konsole before we have a replacement
           plasma-browser-integration
           #print-manager
-        ];
-      };
-    };
-
+    ];
     # see https://nixos.wiki/wiki/KDE#GTK_themes_are_not_applied_in_Wayland_applications
     programs.dconf.enable = true;
 
