b03a8fb9f70828634513070d35413243836e3cbf
alexanderwallau <alexander.wallau@gmx.net>
2023-07-29 21:34:15 +0200

    htop show cpu temp

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -99,6 +99,7 @@ in
       enable = true;
       settings = {
         show_cpu_frequency = true;
+        show_cpu_temprature = true;
         show_cpu_usage = true;
         show_program_path = true;
         tree_view = false;
