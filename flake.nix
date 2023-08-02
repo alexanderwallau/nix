8f8e6b7c8291ec1cabdd39704387ec862264b21f
MayNiklas <info@niklas-steffen.de>
2023-08-02 16:42:25 +0200

    fix change

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
             # allows to only pass what is needed to each module.
             specialArgs = { flake-self = self; } // inputs;
 
-            system = import "./machines" + "/${x}/arch.nix";
+            system = import ./machines/${x}/arch.nix;
 
             modules = [
               (./machines + "/${x}/configuration.nix")
