a7c347081fbac2bed637ac4764615d0863bd1e84
alexanderwallau <alexander.wallau@gmx.net>
2023-08-14 08:18:52 +0200

    init logiops

---
+++ modules/logiops/default.nix
@@ -0,0 +1,35 @@
+{ lib, config, pkgs, ... }:
+with lib;
+let
+  cfg = config.awallau.logiops;
+in
+{
+  options.awallau.logiops = {
+    enable = mkEnableOption "Logiops";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.logiops ];
+
+    environment.etc."logid.cfg".source = ./logid.cfg;
+
+    systemd.services.logid = {
+      enable = true;
+      description = "Logitech configuration daemon";
+      unitConfig = {
+        StartLimitIntervalSec = "0";
+        After = "multi-user.target";
+        Wants = "multi-user.target";
+      };
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.logiops}/bin/logid";
+        User = "root";
+        ExecReload = "/bin/kill -HUP $MAINPID";
+        Restart = "on-failure";
+      };
+      wantedBy = [ "graphical.target" ];
+      partOf = [ "graphical.target" ];
+    };
+  };
+}
