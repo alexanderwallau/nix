5d537b9a3ed676cc6f4cca3dc3d48709bc118285
alexanderwallau <alexander.wallau@gmx.net>
2023-08-31 18:56:08 +0200

    Enabled http for webservices on phelps

--- machines/phelps/configuration.nix
+++ machines/phelps/configuration.nix
@@ -39,6 +39,8 @@
       wget
       git
     ];
+  
+  networking = { firewall = { allowedTCPPorts = [ 443 80 ]; }; };
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
   networking.hostName = "phelps";
 }
