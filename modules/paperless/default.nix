d0f0b43218fb9666d212fdee338497e9dd3c98db
alexanderwallau <alexander.wallau@gmx.net>
2023-10-31 08:26:15 +0100

    Mae paerless only availible via wg

--- modules/paperless/default.nix
+++ modules/paperless/default.nix
@@ -29,6 +29,8 @@ in {
           proxyPass = "http://127.0.0.1:28981/";
           extraConfig = ''
             client_max_body_size 256M;
+            allow 192.168.69.0/24;
+          deny all;
           '';
         };
       };
