f16329df8b5152fa0aa31c460a3bdce68aa2118c
alexanderwallau <alexander.wallau@gmx.net>
2023-12-08 15:31:38 +0100

    Music related python packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,7 @@
   # left for future reference
   todoist-electron = pkgs.callPackage ./todoist-electron { };
   wait-for-build = pkgs.callPackage ./wait-for-build { };
+
+  lyricsgenius = pkgs.callPackage ./lyricsgenius { inherit (pkgs) python3; };
+  tidal-dl = pkgs.callPackage ./tidal-dl { inherit (pkgs) python3; inherit lyricsgenius; };
 }
