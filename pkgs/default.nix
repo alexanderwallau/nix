125912ea83ae41c5d7f13bc2a953b5ce00f02c62
alexanderwallau <alexander.wallau@gmx.net>
2023-12-11 13:53:59 +0100

    Fritzbox exporter Package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,4 +5,6 @@
 
   lyricsgenius = pkgs.callPackage ./lyricsgenius { inherit (pkgs) python3; };
   tidal-dl = pkgs.callPackage ./tidal-dl { inherit (pkgs) python3; inherit lyricsgenius; };
+
+  frizbox-exporter = pkgs.callPackage ./frizbox-exporter { };
 }
