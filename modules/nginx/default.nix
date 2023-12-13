9dfc0851dcf591ff5fada33ec76a6ca3fc6474f8
alexanderwallau <alexander.wallau@gmx.net>
2023-12-13 13:30:41 +0100

    Logs and security for default nginx module

--- modules/nginx/default.nix
+++ modules/nginx/default.nix
@@ -25,10 +25,28 @@ in
       recommendedOptimisation = true;
       recommendedTlsSettings = true;
       recommendedProxySettings = true;
+      recommendedGzipSettings = true;
+      recommendedZstdSettings = true;
+
 
       # lets be more picky on our ciphers and protocols
       sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:+SHA1:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
       sslProtocols = "TLSv1.3 TLSv1.2";
+      # Logs  
+      commonHttpConfig = ''
+        map $remote_addr $remote_addr_anon {
+          ~(?P<ip>\d+\.\d+\.\d+)\.    $ip.0;
+          ~(?P<ip>[^:]+:[^:]+):       $ip::;
+          default                     0.0.0.0;
+        }
+
+        log_format combined_anon '$remote_addr_anon - $remote_user [$time_local] '
+                                 '"$request" $status $body_bytes_sent '
+                                 '"$http_referer" "$http_user_agent"';
+
+        access_log /var/log/nginx/access.log combined_anon buffer=32k flush=5m;
+      '';
+
 
       # both lines can help if errors occur
       # especially with using longer paths
