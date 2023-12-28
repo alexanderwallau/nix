148ec14a1e452165a883007999b5de0c889e0d11
alexanderwallau <alexander.wallau@gmx.net>
2023-12-28 20:08:30 +0100

    Refined nginx config for static sites

--- machines/mayer/static-sites.nix
+++ machines/mayer/static-sites.nix
@@ -1,18 +1,28 @@
-{ self, ... }:
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-  services.nginx.virtualHosts = {
-    # Uses h5ai for a nice frontend 
-    # h5ai not nixed yet need to manually config php
-    "pdok.alexanderwallau.de" = {
-      addSSL = true;
-      enableACME = true;
-      root = "/var/www/pdok.alexanderwallau.de";
-    };
-    "alexanderwallau.de" = {
-      addSSL = true;
-      enableACME = true;
-      root = "/var/www/alexanderwallau.de";
+  services.nginx = {
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      # Uses h5ai for a nice frontend 
+      # h5ai not nixed yet need to manually config php
+      # More or less a giant Fileserver
+      "pdok.alexanderwallau.de" = {
+        addSSL = true;
+        enableACME = true;
+        root = "/var/www/pdok.alexanderwallau.de";
+        locations."/" = {
+          tryFiles = "$uri $uri/ index.php";
+          index = "index.php index.html index.htm";
+        };
+      };
+      # alexanderwallau.de a staticly generated site with hugo auto refreshed with deploy-hugo
+      "alexanderwallau.de" = {
+        addSSL = true;
+        enableACME = true;
+        root = "/var/www/alexanderwallau.de";
+      };
     };
   };
 }
