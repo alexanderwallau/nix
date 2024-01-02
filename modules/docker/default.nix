c9259c2afed4dd88d11f10a5d3cd39d8718d26af
alexanderwallau <alexander.wallau@gmx.net>
2024-01-02 11:26:33 +0100

    Fix Docker Network for ICE Trains

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -12,6 +12,11 @@ in
 
     virtualisation.docker = {
       enable = true;
+      # for ICE wifi, ref https://gist.github.com/sunsided/7840e89ff4e11b64a2d7503fafa0290c
+      extraOptions = lib.concatStringsSep " " [
+        "--bip=172.39.1.5/24"
+        "--fixed-cidr=172.39.1.0/25"
+      ];
       autoPrune = {
         enable = true;
         dates = "weekly";
