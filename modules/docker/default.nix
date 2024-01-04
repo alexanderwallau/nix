300aa50933fa3d520c8a87611baaf055e4570c56
alexanderwallau <alexander.wallau@gmx.net>
2024-01-04 09:53:42 +0100

    Change default oci backend to Docker

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -10,18 +10,21 @@ in
 
     environment.systemPackages = with pkgs; [ docker-compose ];
 
-    virtualisation.docker = {
-      enable = true;
-      # for ICE wifi, ref https://gist.github.com/sunsided/7840e89ff4e11b64a2d7503fafa0290c
-      extraOptions = lib.concatStringsSep " " [
-        "--bip=172.39.1.5/24"
-        "--fixed-cidr=172.39.1.0/25"
-      ];
-      autoPrune = {
+    virtualisation = {
+      docker = {
         enable = true;
-        dates = "weekly";
-      };
+        # for ICE wifi, ref https://gist.github.com/sunsided/7840e89ff4e11b64a2d7503fafa0290c
+        extraOptions = lib.concatStringsSep " " [
+          "--bip=172.39.1.5/24"
+          "--fixed-cidr=172.39.1.0/25"
+        ];
+        autoPrune = {
+          enable = true;
+          dates = "weekly";
+        };
 
+      };
+      oci-containers.backend = "docker";
     };
 
   };
--- modules/podman/default.nix
+++ modules/podman/default.nix
@@ -10,7 +10,6 @@ in {
   };
   config = mkIf cfg.enable {
     virtualisation = {
-      oci-containers.backend = "podman";
       podman = {
         enable = true;
         defaultNetwork.settings.dns_enabled = true;
