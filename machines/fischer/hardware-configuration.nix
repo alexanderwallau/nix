d183b935da4a3f1d51e3eb4053907cde33ec293d
Alexander Wallau <alexander.wallau@gmx.net>
2024-03-11 16:44:21 +0100

    Correct conflicting options

--- machines/fischer/hardware-configuration.nix
+++ machines/fischer/hardware-configuration.nix
@@ -104,5 +104,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  services.tlp.enable = true;
 }
