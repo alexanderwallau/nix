faddd34dcf11fa553e8425c94ee58c82af53a43b
Alexander Wallau <alexander.wallau@gmx.net>
2024-09-09 18:31:46 +0200

    Audiobooks do have some size, apparently

--- modules/audiobookshelf/default.nix
+++ modules/audiobookshelf/default.nix
@@ -34,6 +34,14 @@ in
       nginx.virtualHosts."${cfg.domain}" = {
         enableACME = true;
         forceSSL = true;
+         extraConfig = ''
+        # Allow any size file to be uploaded
+        # Exception here - audiobooks are large and scp is not a option for every user
+        # Set to a value such as 1000m; to restrict file size to a specific value
+        client_max_body_size 0;
+        # To disable buffering
+        proxy_buffering off;
+        '';
         locations."/" = {
           proxyPass = "http://127.0.0.1:${builtins.toString cfg.port}";
           proxyWebsockets = true;
