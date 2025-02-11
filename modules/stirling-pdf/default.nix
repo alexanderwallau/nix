274e1d9d4b3408a6d44d6e89cf0a87f327bd31e3
Alexander Wallau <alexander.wallau@gmx.net>
2025-02-11 20:38:38 +0100

    new service stirlingpdf

---
+++ modules/stirling-pdf/default.nix
@@ -0,0 +1,39 @@
+{ config, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.awallau.stirling-pdf;
+in {
+  options.awallau.stirling-pdf = {
+    enable = mkEnableOption "activate stirling-pdf";
+    
+    port = mkOption {
+      type = types.int;
+      default = 7264;
+      description = "port to run the application on";
+    };
+    domain = mkOption {
+      type = types.str;
+      default = "stir.ling.pdf";
+      description = "domain name for stirling-pdf";
+    };
+  }
+
+  config = mkIf cfg.enable {
+    services = {
+      stirling-pdf = {
+        enable = true;
+        environment = { 
+          SERVER_PORT = cfg.port; 
+          };
+      };
+      nginx.virtualHosts = {
+          "${cfg.domain}" = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/" = {
+            proxyPass = "http://127.0.0.1:${toString cfg.port}";
+            };
+          };
+        };
+    };
+  };
+}
