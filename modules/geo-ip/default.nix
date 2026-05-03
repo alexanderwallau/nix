e9d7c32d17cd234cc98dc88cdea35f1230379ddc
Alexander Wallau <alexander.wallau@gmx.net>
2026-05-03 23:07:57 +0200

    Refactor geo-ip

--- modules/geo-ip/default.nix
+++ modules/geo-ip/default.nix
@@ -1,29 +1,91 @@
-{ config, lib, pkgs, ... }:
-with lib;
-let cfg = config.awallau.nginx; in
 {
-
-  imports = [
-    ./locationOptions.nix
-    ./vhostOptions.nix
-  ];
-
-  options.awallau.nginx = {
-    geoIP = mkEnableOption "enable GeoIP";
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.awallau.geo-ip;
+  enabledVhosts = (
+    builtins.filter (vhost: vhost.value.enableGeoIP) (
+      lib.attrsToList config.services.nginx.virtualHosts
+    )
+  );
+  enabledLocations = (
+    builtins.concatMap (
+      vhost: builtins.filter (loc: loc.value.enableGeoIP) (lib.attrsToList vhost.value.locations)
+    ) (lib.attrsToList config.services.nginx.virtualHosts)
+  );
+  enable =
+    (!cfg.forceDisable)
+    # auto-enable when at least one vhost or location has the option set
+    && ((builtins.length enabledVhosts) != 0 || (builtins.length enabledLocations) != 0);
+in
+{
+  options.awallau.geo-ip = {
+    forceDisable = lib.mkEnableOption "force disable geo-ip systemwide";
   };
 
-  config = mkIf cfg.geoIP {
+  options.services.nginx.virtualHosts = lib.mkOption {
+    type = lib.types.attrsOf (
+      lib.types.submodule (
+        { config, ... }:
+        {
+          options = {
+            enableGeoIP = lib.mkEnableOption "enable geo-ip for this vhost";
+          };
+          config.extraConfig = toString [
+            (lib.optional (config.enableGeoIP && !cfg.forceDisable) ''
+              if ($allowed_country = no) {
+                return 444;
+              }
+            '')
+          ];
 
-    # when Nginx is enabled, enable the GeoIP updater service
-    services.geoipupdate = mkIf cfg.enable {
+          options.locations = lib.mkOption {
+            type = lib.types.attrsOf (
+              lib.types.submodule (
+                { config, ... }:
+                {
+                  options = {
+                    enableGeoIP = lib.mkEnableOption "enable geo-ip";
+                  };
+                  config.extraConfig = toString [
+                    (lib.optional (config.enableGeoIP && !cfg.forceDisable) ''
+                      if ($allowed_country = no) {
+                        return 444;
+                      }
+                    '')
+                  ];
+                }
+              )
+            );
+          };
+        }
+      )
+    );
+  };
+
+  config = lib.mkIf enable {
+sops.secrets = {
+      "maxmind-licence-key" = { };
+    };
+
+    services.geoipupdate = {
       enable = true;
       interval = "weekly";
       settings = {
-        EditionIDs = [ "GeoLite2-Country" ];
-        AccountID = 545115;
-        LicenseKey = "/var/src/secret/maxmind_license_key";
-        DatabaseDirectory = "/var/lib/GeoIP";
+        AccountID = 1042214;
+
+        EditionIDs = [
+          "GeoLite2-ASN"
+          "GeoLite2-City"
+          "GeoLite2-Country"
+        ];
+
+        LicenseKey = config.age.secrets.geoipupdate-license.path;
       };
+
     };
 
     # build nginx with geoip2 module
@@ -32,33 +94,33 @@ let cfg = config.awallau.nginx; in
         modules = with pkgs.nginxModules; [ geoip2 ];
         buildInputs = oldAttrs.buildInputs ++ [ pkgs.libmaxminddb ];
       });
-      appendHttpConfig = toString (
-        [
-          # we want to load the geoip2 module in our http config, pointing to the database we are using
-          # country iso code is the only data we need
-          ''
-            geoip2 ${config.services.geoipupdate.settings.DatabaseDirectory}/GeoLite2-Country.mmdb {
-              $geoip2_data_country_iso_code country iso_code;
-            }
-          ''
-          # we want to allow only requests from specific countries
-          # if a request is not from such a country, we return no, which will result in a 403
-          ''
-            map $geoip2_data_country_iso_code $allowed_country {
-              default no;
-              DE yes;
-              FR yes;
-              GB yes;
-              IT yes;
-              NL yes;
-              AT yes;
-              BE yes;
-              DK yes;
-            }
-          ''
-        ]
-      );
+      appendHttpConfig = toString [
+        # we want to load the geoip2 module in our http config, pointing to the database we are using
+        # the country iso code is the only data we need
+        ''
+          geoip2 ${config.services.geoipupdate.settings.DatabaseDirectory}/GeoLite2-Country.mmdb {
+            $geoip2_data_country_iso_code country iso_code;
+          }
+        ''
+        # we want to allow only requests from specific countries
+        # if a request is not from such a country, we return no, which will result in a 403
+        ''
+          map $geoip2_data_country_iso_code $allowed_country {
+            default no;
+            DE yes;
+            AT yes; 
+            ES yes;
+            FR no; 
+            GB yes;
+            IT yes;
+            NL yes;
+            JP yes;
+            LU yes;
+            US yes;
+            AU yes;
+          }
+        ''
+      ];
     };
-
   };
 }
