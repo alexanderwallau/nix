745918897ae7fd9096c47f07cb717f0ee5962209
Alexander Wallau <alexander.wallau@gmx.net>
2026-06-01 15:30:55 +0200

    migrate hedgedoc to use SSO & Postgres

--- modules/hedgedoc/default.nix
+++ modules/hedgedoc/default.nix
@@ -12,6 +12,15 @@ in {
     };
   };
   config = mkIf cfg.enable {
+
+    sops.secrets = {
+      "hedgedoc-oidc-secret" = { 
+        # For reasons this needs to be very explicitly set, using clan would make this more elegrant
+        # I digress
+        owner = config.systemd.services.hedgedoc.serviceConfig.User;
+      };
+    };
+    
     services = {
       hedgedoc = {
         enable = true;
@@ -19,14 +28,42 @@ in {
           domain = "${cfg.domain}";
           host = "127.0.0.1";
           port = 3400;
+          environmentFile = config.sops.secrets."hedgedoc-oidc-secret".path;
+
           protocolUseSSL = true;
           useSSL = false;
-          allowGravatar = true;
-          allowFreeURL = true;
+
+          allowGravatar = false;
+          allowAnonymous = false;
+          allowAnonymousEdits = true;
+          allowAnonymousUploads = true;
+          allowFreeUrl = true;
+          requireFreeURLAuthentication = true;
+        
+          defaultPermissions = "limited";
+
           db = {
-            dialect = "sqlite";
-            storage = "/var/lib/hedgedoc/db.sqlite";
+          dialect = "postgres";
+          host = "/run/postgresql/";
           };
+        # Fun with Auth 
+        email = false;
+        oauth2 = {
+          baseUrl = "https://sso.alexanderwallau.de";
+          providerName = "AlexanderWallau.de";
+          clientID = "hedgedoc";
+          # This gets replacesd at runtime or so
+          clientSecret = "$OIDC_SECRET";
+          scope = "openid email profile";
+          # Or to be fail reading the keycloak docs and taking -2 intelligent guesses
+          userProfileURL = "https://sso.alexanderwallau.de/realms/alexanderwallau/protocol/openid-connect/userinfo";
+          tokenURL = "https://sso.alexanderwallau.de/realms/alexanderwallau/protocol/openid-connect/token";
+          authorizationURL = "https://sso.alexanderwallau.de/realms/alexanderwallau/protocol/openid-connect/auth";
+          userProfileUsernameAttr = "preferred_username";
+          userProfileDisplayNameAttr = "name";
+          userProfileEmailAttr = "email";
+        };
+
         };
       };
       nginx.virtualHosts."${cfg.domain}" = {
@@ -34,9 +71,30 @@ in {
         forceSSL = true;
         locations."/" = {
           proxyPass = "http://127.0.0.1:3400";
+          proxyWebsockets = true;
         };
+        locations."/robots.txt" = {
+            extraConfig = ''
+              add_header  Content-Type  text/plain;
+              return 200 "User-agent: *\nDisallow: /\n";
+            '';
+          };
 
       };
+      # Move to postgres
+      postgresql = {
+      ensureUsers = [
+        {
+          name = "hedgedoc";
+          ensureDBOwnership = true;
+        }
+      ];
+      ensureDatabases = [ "hedgedoc" ];
     };
   };
+
+  systemd.services.hedgedoc.preStart = lib.mkBefore ''
+    export OIDC_SECRET="$(cat ${config.sops.secrets."hedgedoc-oidc-secret".path})"
+  '';
+};
 }
