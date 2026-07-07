d6864e359d240b02601f6d2f980741daeb6ed171
Alexander Wallau <alexander.wallau@gmx.net>
2026-07-07 08:54:00 +0200

    Add significant umpf to postgres

--- machines/mayer/hardware-configuration.nix
+++ machines/mayer/hardware-configuration.nix
@@ -30,6 +30,27 @@
     options = [ "nosuid" "nodev" "relatime" "size=2G" ];
   };
 
+
+  # Some optimized Postgres Parameters
+  services.postgresql.settings = {
+    max_connections = 100;
+    shared_buffers = "4GB";
+    effective_cache_size = "8GB";
+    maintenance_work_mem = "1GB";
+    checkpoint_completion_target = 0.9;
+    wal_buffers = "16MB";
+    default_statistics_target = 100;
+    random_page_cost = 1.1;
+    effective_io_concurrency = 200;
+    work_mem = "19418kB";
+    huge_pages = "off";
+    min_wal_size = "1GB";
+    max_wal_size = "4GB";
+    max_worker_processes = 8;
+    max_parallel_workers_per_gather = 4;
+    max_parallel_workers = 8;
+    max_parallel_maintenance_workers = 4;
+  };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
 }
